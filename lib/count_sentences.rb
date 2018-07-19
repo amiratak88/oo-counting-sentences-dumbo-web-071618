a0ed2572bf2b9727e63d5e98f789d18a6eb9f9cf
github username <github email address>
2018-07-19 11:58:19 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    i = 0
+    self.each_char.with_index do |char, idx|
+      i += 1 if [".", "?", "!"].include?(char) && ![".", "?", "!"].include?(self[idx - 1])
+    end
+    i
   end
 end
